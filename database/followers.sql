3ccb82f73e393cd8de803dd2bdd99dae13f4ada9
I-Info <49438478+I-Info@users.noreply.github.com>
2021-12-03 21:01:35 +0800

    add followers.sql

--- database/followers.sql
+++ database/followers.sql
@@ -1,12 +1,11 @@
 create table followers
 (
     uid          int not null,
-    follower_uid int not null
+    follower_uid int not null,
+    constraint followers_uid_follower_uid_uindex
+        unique (uid, follower_uid)
 );
 
 create index followers_follower_uid_index
     on followers (follower_uid);
-
-create index followers_uid_index
-    on followers (uid);
 
