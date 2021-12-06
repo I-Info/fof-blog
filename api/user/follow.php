ff40171301f3952f319bc6034f281e6d92ba932e
I-Info <49438478+I-Info@users.noreply.github.com>
2021-12-06 14:03:28 +0800

    remove follow transaction & lock

--- api/user/follow.php
+++ api/user/follow.php
@@ -13,42 +13,27 @@ if (!isset($data->uid) || !is_numeric($data->uid))
     die(http_bad_request());
 
 $id = $data->uid; // the user to be followed
+if ($id == $uid)
+    die(http_bad_request("cannot follow yourself"));
 
 $conn = db_connect();
-// positive lock
-for ($i = 0; $i < 5; ++$i) {
-    // mysql transaction
-    if ($conn->begin_transaction() == false)
-        die(http_server_error("transaction fail"));
-    try {
-        $stmt = $conn->prepare("SELECT `update_time` AS `t` FROM `users` WHERE `id` = ?;");
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        if (!$result || !$result->num_rows) {
-            die(http_not_found());
-        }
-        $time = $result->fetch_assoc()['t'];
 
-        $stmt = $conn->prepare("INSERT INTO `followers` (`uid`, `follower_uid`) VALUES (?, ?);");
-        $stmt->bind_param("ii", $id, $uid);
-        $stmt->execute();
+try {
+    $stmt = $conn->prepare("INSERT INTO `followers` (`uid`, `follower_uid`) VALUES (?, ?);");
+    $stmt->bind_param("ii", $id, $uid);
+    $stmt->execute();
 
-        $stmt = $conn->prepare("UPDATE `users` SET `followers` = `followers` + 1 WHERE `id` = ? AND `update_time` = ?;");
-        $stmt->bind_param("is", $id, $time);
-        $stmt->execute();
-        if ($stmt->affected_rows > 0) {
-            $conn->commit();
-            exit(http_ok());
-        } else {
-            $conn->rollback();
-        }
-    } catch (mysqli_sql_exception $exception) {
-        $conn->rollback();
-        global $DEBUG;
-        if ($exception->getCode() == 1062)
-            exit(http_forbidden("already followed"));
-        exit(http_bad_request($DEBUG ? "c:" . $exception->getCode() . "m:" . $exception->getMessage() : "bad request"));
+    $stmt = $conn->prepare("UPDATE `users` SET `followers` = `followers` + 1 WHERE `id` = ?;");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    if ($stmt->affected_rows > 0) {
+        exit(http_ok());
     }
+    exit(http_server_error());
+} catch (mysqli_sql_exception $exception) {
+    global $DEBUG;
+    if ($exception->getCode() == 1062)
+        exit(http_forbidden("already followed"));
+    exit(http_bad_request($DEBUG ? "c:" . $exception->getCode() . "m:" . $exception->getMessage() : "bad request"));
 }
-echo http_server_error("time out");
+
--- api/user/unfollow.php
+++ api/user/unfollow.php
@@ -16,41 +16,23 @@ $id = $data->uid; // the user to be followed
 
 $conn = db_connect();
 // positive lock
-for ($i = 0; $i < 5; ++$i) {
-    try {
-        $stmt = $conn->prepare("SELECT `update_time` AS `t` FROM `users` WHERE `id` = ?;");
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        if (!$result || !$result->num_rows) {
-            die(http_not_found());
-        }
-        $time = $result->fetch_assoc()['t'];
-
-        // mysql transaction
-        if ($conn->begin_transaction() == false)
-            die(http_server_error("transaction fail"));
-
-        $stmt = $conn->prepare("DELETE FROM `followers` WHERE `uid` = ? AND `follower_uid` = ?;");
-        $stmt->bind_param("ii", $id, $uid);
-        $stmt->execute();
-        if ($stmt->affected_rows <= 0) {
-            exit(http_not_found());
-        }
-
-        $stmt = $conn->prepare("UPDATE `users` SET `followers` = `followers` - 1 WHERE `id` = ? AND `update_time` = ?;");
-        $stmt->bind_param("is", $id, $time);
-        $stmt->execute();
-        if ($stmt->affected_rows > 0) {
-            $conn->commit();
-            exit(http_ok());
-        } else {
-            $conn->rollback();
-        }
-    } catch (mysqli_sql_exception $exception) {
-        $conn->rollback();
-        global $DEBUG;
-        exit(http_bad_request($DEBUG ? "c:" . $exception->getCode() . "m:" . $exception->getMessage() : "bad request"));
+try {
+    $stmt = $conn->prepare("DELETE FROM `followers` WHERE `uid` = ? AND `follower_uid` = ?;");
+    $stmt->bind_param("ii", $id, $uid);
+    $stmt->execute();
+    if ($stmt->affected_rows <= 0) {
+        exit(http_not_found());
     }
+
+    $stmt = $conn->prepare("UPDATE `users` SET `followers` = `followers` - 1 WHERE `id` = ?;");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    if ($stmt->affected_rows) {
+        exit(http_ok());
+    }
+    die(http_server_error());
+} catch (mysqli_sql_exception $exception) {
+    $conn->rollback();
+    global $DEBUG;
+    exit(http_bad_request($DEBUG ? "c:" . $exception->getCode() . "m:" . $exception->getMessage() : "bad request"));
 }
-echo http_server_error("time out");
